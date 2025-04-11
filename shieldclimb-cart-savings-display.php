d3ed04d66722ff434d5a62f73ac06dedc85bdd95
ShieldClimb <contact@shieldclimb.com>
2025-04-11 15:37:44 +0800

    Updated to version 1.0.2

--- shieldclimb-cart-savings-display.php
+++ shieldclimb-cart-savings-display.php
@@ -4,12 +4,12 @@
  * Plugin Name: ShieldClimb – Cart Savings Display for WooCommerce
  * Plugin URI: https://shieldclimb.com/free-woocommerce-plugins/cart-savings-display/
  * Description: Cart Savings Display for WooCommerce shows customers their total savings in the cart & checkout, boosting conversions and purchase confidence.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Requires Plugins: woocommerce
  * Requires at least: 5.8
- * Tested up to: 6.7
+ * Tested up to: 6.8
  * WC requires at least: 5.8
- * WC tested up to: 9.7.1
+ * WC tested up to: 9.8.1
  * Requires PHP: 7.2
  * Author: shieldclimb.com
  * Author URI: https://shieldclimb.com/about-us/
